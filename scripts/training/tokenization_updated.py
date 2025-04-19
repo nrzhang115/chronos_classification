4dd9697befa2d9d67b7d99d37fba735b340b2e56
nrzhang115 <159680727+nrzhang115@users.noreply.github.com>
2025-04-19 22:03:22 +1000

    run on srv

--- scripts/training/tokenization_updated.py
+++ scripts/training/tokenization_updated.py
@@ -1,4 +1,8 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "src")))
+#####
+#import os
 import pyarrow as pa
 import pyarrow.ipc as ipc
 import torch
